11cb7f8dd9965e31f1166d1fe43f478513b5f373
Denislav Marinov <58664415+denislavmarinov@users.noreply.github.com>
2024-04-20 11:07:24 +0300

    Define addNode function
    The addNode function is responsible for addition of new elements to the linked list.

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// Define the struucture
 struct Node
 {
     int data;
@@ -8,7 +9,58 @@ struct Node
 
 typedef struct Node SN;
 
+// Globally create the struct beggining
+SN *head = NULL;
+
+//Define the functions
+void addNode(int data);
+
+
 int main()
 {
 
+}
+
+/*
+* Method for adding new element to the list
+* params:
+*   - int data - the data that is to be entered
+* return: None
+*/
+void addNode(int data)
+{
+    SN *temp = NULL, *newNode = NULL;
+    // Check if the linked list is empty
+    if (head == NULL)
+    {
+        // Allocate dynamically memory
+        head = (SN*)malloc(sizeof(SN));
+        // Check if the memory is allocated
+        if (head != NULL)
+        {
+            // Save the data
+            head->data = data;
+            head->next = NULL;
+        }
+    }
+    else
+    {
+        // Assign to temp the address of the last element of the list
+        while(temp->next != NULL)
+        {
+            // Go to the next element
+            temp = temp->next;
+        }
+        // Dynamically allocate memory for the new element
+        newNode = (SN*)malloc(sizeof(SN));
+        // Check if the memory is allocated
+        if (newNode != NULL)
+        {
+            // Save the data
+            newNode->data = data;
+            newNode->next = NULL;
+            // Add the link for the new to the previous one
+            temp->next = newNode;
+        }
+    }
 }
